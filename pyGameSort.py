e1beb771f1a0fbed0c9b7f01c6825d841f87fdc9
hamidrezaMomeni <momenih333@gmail.com>
2019-08-24 20:10:19 +0430

    upload version 1.0

---
+++ pyGameSort.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python3
+"""
+===========================================================================
+
+    ***************************************************
+    ***************** ACCIDENT ESCAPE *****************
+    ***************************************************
+    ***************************************************
+    ***************** HamidReza Momeni ****************
+    ***************************************************
+
+    Game Name: Accident Escape        
+    Game Author: HamidReza Momeni
+    Game Version: 0.1
+    Game Discription: Accident Escape is a simple and fun game with python
+    Game Github: https://github.com/hamidrezaMomeni/AccidentEscape
+    Author Github: https://github.com/hamidrezaMomeni
+    Author Instagram: https://instagram.com/hamidreza_momeni_
+    Author Telegram: https://t.me/hamidrezaPK7
+
+============================================================================
+"""
+import pygame
+import random
+import time
+# start pygame
+pygame.init()
+# set width and hieght for display window
+width = 900
+height = 550
+# set colors
+black = (0,0,0)
+white = (255,255,255)
+red = (200,0,0)
+orange = (255, 100, 0)
+blue = (0, 83, 255)
+yellow = (245, 219, 0)
+green = (0,240,0)
+lightGreen = (0,230,0)
+purple = (148, 3, 252)
+pink = (252, 3, 140)
+colorList = [green, pink, red, blue, purple, yellow, orange]
+# create game display and caption for title window and clock
+gameDisplay = pygame.display.set_mode((width, height))
+pygame.display.set_caption("AccidentEscape Game")
+clock = pygame.time.Clock()
+# get and load car image
+carImg = pygame.image.load("Bug.png")
+# this function drawing a rect object and insert into game display
+def stuff(stuffX, stuffY, stuffW, stuffH, color):
+    pygame.draw.rect(gameDisplay,color,[stuffX, stuffY, stuffW, stuffH])
+# set x,y position car for show to game display
+def carPosition(x,y):
+    gameDisplay.blit(carImg, (x,y))
+# get render and return text with your font selection
+def textObjects(text,fonts):
+    textSurface = fonts.render(text, True, black)
+    return textSurface,textSurface.get_rect()
+# stop game and show message and run game again
+def messageDisplay(msg):
+    # select font family and font size for show to user
+    largText = pygame.font.Font("freesansbold.ttf", 50)
+    textSurf, textRect = textObjects(msg, largText)
+    textRect.center = ((width/2), (height/2))
+    gameDisplay.blit(textSurf, textRect)
+    pygame.display.update()
+    time.sleep(4)
+    gameLoop()
+# when user accidented stop game and show message
+def crash():
+    messageDisplay("you Crashed !!")
+# show on top display user score
+def score(count):
+    font = pygame.font.SysFont(None, 25)
+    text = font.render("Score: {}".format(str(count)), True, black)
+    gameDisplay.blit(text, (1,1))
+# welcome screen to show user
+def gameIntro():
+    intro = True
+    while intro == True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()                
+        gameDisplay.fill(white)
+        largText = pygame.font.Font("freesansbold.ttf", 50)
+        textSurf, textRect = textObjects("Game Starting, You Ready?", largText)
+        textRect.center = ((width/2), (height/2))
+        gameDisplay.blit(textSurf, textRect)
+        mouse = pygame.mouse.get_pos()
+        #if 150 + 100 > mouse[0] 
+        # button for start or quit from game
+        pygame.draw.rect(gameDisplay, green, (200,450,100,50))
+        pygame.draw.rect(gameDisplay, red, (600,450,100,50))
+        pygame.display.update()
+# Game components loop
+bestScore = 0
+def gameLoop():
+    # set y position and random x position for car and another information
+    xPosRange = (0.25,0.45,0.65,0.85)
+    xPosRand = random.choice(xPosRange)
+    xPosition = (width * xPosRand) 
+    yPosition = (height * 0.85)
+    xChange = 0
+    counter = 0
+    carWidth = 32
+    global bestScore
+    # set some variable for generate stuff
+    stuffStartX = random.randrange(0,width)
+    stuffStartY = -600
+    stuffSpeed = 7
+    stuffWidth = 100
+    stuffHeight = 100
+    stuffColor = random.choice(colorList)
+    # set default state for user
+    gameExit = False
+    # An endless loop until the user loss
+    while not gameExit:
+        # A loop to capture all the events while playing
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    xChange = -5
+                elif event.key == pygame.K_RIGHT:
+                    xChange = 5
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    xChange = 0
+        # apply x change for car
+        xPosition += xChange
+        # filling screen with white color
+        gameDisplay.fill(white)
+        # show car in x,y Position 
+        carPosition(xPosition, yPosition)
+        # show user score in the top window
+        score(counter)
+        # generate stuff and scroll down from sky
+        stuff(stuffStartX,stuffStartY,stuffWidth,stuffHeight,stuffColor)
+        stuffStartY += stuffSpeed
+        # when car accidented with left and right wall
+        if xPosition > width - 32 or xPosition < 0:
+            crash()
+        # rain stuff from sky and calculate user score
+        if stuffStartY > height:
+            stuffStartY = 0 - stuffHeight
+            stuffStartX = random.randrange(0,width)
+            stuffColor = random.choice(colorList)
+            counter += 1
+            # if score 5 level upgraded speed stuff up to +1
+            if counter % 5 == 0:
+                stuffSpeed += 1
+        # when accident stuff with car
+        
+        font = pygame.font.SysFont(None, 30)
+        text = font.render("Best Score: {}".format(str(bestScore)), True, green)
+        gameDisplay.blit(text, (750,1))
+        pygame.display.update()
+        
+        
+        if yPosition < stuffStartY + stuffHeight:
+            if xPosition > stuffStartX and xPosition < stuffStartX + stuffWidth or xPosition + carWidth > stuffStartX and xPosition + carWidth < stuffStartX + stuffWidth:
+                if counter > bestScore:
+                    bestScore = counter
+                crash()
+        # game display updating
+        pygame.display.update()
+        # run game with 80 frame/second
+        clock.tick(80)
+# running game form the first time
+#gameIntro()
+gameLoop()
+# quit from pygame library and python file
+pygame.quit()
+quit()
+
+
